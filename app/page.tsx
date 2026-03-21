e392fa0911b1bc4986ca04cc3e75d46791029c84
Julien Capellari <julien.capellari@gmail.com>
2026-03-21 14:16:58 +0100

    feat: display grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+const grid = [
+  ['A', 'B', 'C', 'D', 'E', 'F'],
+  ['G', 'H', 'I', 'J', 'K', 'L'],
+  ['M', 'N', 'O', 'P', 'Q', 'R'],
+  ['S', 'T', 'U', 'V', 'W', 'X'],
+  ['X', 'Y', 'Z', 'A', 'B', 'C'],
+  ['D', 'E', 'F', 'G', 'H', 'I'],
+]
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="grid auto-cols-auto auto-rows-auto w-fit m-auto select-none">
+      {grid.map((row, y) => row.map((cell, x) => (
+        <div
+          key={`${x}:${y}`}
+          className="size-8 text-center leading-8"
+          style={{ gridRow: y + 1, gridColumn: x + 1 }}
+        >
+          {cell}
+        </div>
+      )))}
+    </div>
   );
 }
