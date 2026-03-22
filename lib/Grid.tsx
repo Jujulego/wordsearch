301f9d0aefaa8d76c948ea67cec0afc1459edfc2
Julien Capellari <julien.capellari@gmail.com>
2026-03-22 15:13:41 +0100

    feat: limit overlay angle & length

--- lib/Grid.tsx
+++ lib/Grid.tsx
@@ -8,11 +8,14 @@ export default function Grid({ className, grid }: GridProps) {
   const [endPoint, setEndPoint] = useState<Point | null>(null);
   const [overlays, setOverlays] = useState<(readonly [Point, Point])[]>([]);
 
+  const rows = grid.length;
+  const cols = grid[0].length;
+
   const handlePointerDown = useCallback((event: PointerEvent<HTMLDivElement>) => {
     const container = event.currentTarget.getBoundingClientRect();
     const point = {
-      x: Math.floor((event.clientX - container.left) / 32),
-      y: Math.floor((event.clientY - container.top) / 32),
+      x: Math.min(Math.max(Math.floor((event.clientX - container.left) / 32), 0), cols - 1),
+      y: Math.min(Math.max(Math.floor((event.clientY - container.top) / 32), 0), rows - 1),
     };
 
     setStartPoint(point);
@@ -20,13 +23,68 @@ export default function Grid({ className, grid }: GridProps) {
   }, []);
 
   const handlePointerMove = useCallback((event: PointerEvent<HTMLDivElement>) => {
+    if (!startPoint) {
+      return;
+    }
+
+    // Compute "hovered" point
     const container = event.currentTarget.getBoundingClientRect();
 
-    setEndPoint({
+    let point = {
       x: Math.floor((event.clientX - container.left) / 32),
       y: Math.floor((event.clientY - container.top) / 32),
-    });
-  }, []);
+    };
+
+    let dx = point.x - startPoint.x;
+    let dy = point.y - startPoint.y;
+
+    // Limit angle
+    const a = Math.round(angle(startPoint, point) / (Math.PI / 4)) * (Math.PI / 4);
+
+    if (Math.abs(a) === Math.PI / 2) {
+      point = {
+        x: startPoint.x,
+        y: Math.min(Math.max(point.y, 0), rows - 1),
+      };
+    } else if (Math.abs(a) === Math.PI || a === 0) {
+      point = {
+        x: Math.min(Math.max(point.x, 0), cols - 1),
+        y: startPoint.y,
+      };
+    } else if (Math.abs(dx) < Math.abs(dy)) {
+      point = {
+        x: point.x,
+        y: startPoint.y + Math.round(dx * Math.tan(a)),
+      };
+    } else {
+      point = {
+        x: startPoint.x + Math.round(dy / Math.tan(a)),
+        y: point.y,
+      };
+    }
+
+    // Limit length
+    dx = point.x - startPoint.x;
+    dy = point.y - startPoint.y;
+
+    if (point.x > cols - 1) {
+      point.x = cols - 1;
+      point.y = startPoint.y + Math.sign(dy) * (cols - 1 - startPoint.x);
+    } else if (point.x < 0) {
+      point.x = 0;
+      point.y = startPoint.y + Math.sign(dy) * startPoint.x;
+    }
+
+    if (point.y > rows - 1) {
+      point.x = startPoint.x + Math.sign(dx) * (rows - 1 - startPoint.y);
+      point.y = rows - 1;
+    } else if (point.y < 0) {
+      point.x = startPoint.x + Math.sign(dx) * startPoint.y;
+      point.y = 0;
+    }
+
+    setEndPoint(point);
+  }, [startPoint]);
 
   const handlePointerUp = useCallback(() => {
     if (startPoint && endPoint) {
