43217cc53f7b43391882cee653a47105ffc05d7c
Julien Capellari <julien.capellari@gmail.com>
2026-03-28 11:40:47 +0100

    chore: replace biome with eslint & prettier

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,8 @@ const grid = [
   ['S', 'T', 'U', 'V', 'W', 'X'],
   ['X', 'Y', 'Z', 'A', 'B', 'C'],
   ['D', 'E', 'F', 'G', 'H', 'I'],
-]
+];
 
 export default function Home() {
-  return (
-    <Grid className="m-auto" grid={grid} />
-  );
+  return <Grid className="m-auto" grid={grid} />;
 }
--- lib/Grid.tsx
+++ lib/Grid.tsx
@@ -11,80 +11,86 @@ export default function Grid({ className, grid }: GridProps) {
   const rows = grid.length;
   const cols = grid[0].length;
 
-  const handlePointerDown = useCallback((event: PointerEvent<HTMLDivElement>) => {
-    const container = event.currentTarget.getBoundingClientRect();
-    const point = {
-      x: Math.min(Math.max(Math.floor((event.clientX - container.left) / 32), 0), cols - 1),
-      y: Math.min(Math.max(Math.floor((event.clientY - container.top) / 32), 0), rows - 1),
-    };
-
-    setStartPoint(point);
-    setEndPoint(point);
-  }, []);
-
-  const handlePointerMove = useCallback((event: PointerEvent<HTMLDivElement>) => {
-    if (!startPoint) {
-      return;
-    }
-
-    // Compute "hovered" point
-    const container = event.currentTarget.getBoundingClientRect();
-
-    let point = {
-      x: Math.floor((event.clientX - container.left) / 32),
-      y: Math.floor((event.clientY - container.top) / 32),
-    };
-
-    let dx = point.x - startPoint.x;
-    let dy = point.y - startPoint.y;
-
-    // Limit angle
-    const a = Math.round(angle(startPoint, point) / (Math.PI / 4)) * (Math.PI / 4);
-
-    if (Math.abs(a) === Math.PI / 2) {
-      point = {
-        x: startPoint.x,
-        y: Math.min(Math.max(point.y, 0), rows - 1),
+  const handlePointerDown = useCallback(
+    (event: PointerEvent<HTMLDivElement>) => {
+      const container = event.currentTarget.getBoundingClientRect();
+      const point = {
+        x: Math.min(Math.max(Math.floor((event.clientX - container.left) / 32), 0), cols - 1),
+        y: Math.min(Math.max(Math.floor((event.clientY - container.top) / 32), 0), rows - 1),
       };
-    } else if (Math.abs(a) === Math.PI || a === 0) {
-      point = {
-        x: Math.min(Math.max(point.x, 0), cols - 1),
-        y: startPoint.y,
+
+      setStartPoint(point);
+      setEndPoint(point);
+    },
+    [rows, cols],
+  );
+
+  const handlePointerMove = useCallback(
+    (event: PointerEvent<HTMLDivElement>) => {
+      if (!startPoint) {
+        return;
+      }
+
+      // Compute "hovered" point
+      const container = event.currentTarget.getBoundingClientRect();
+
+      let point = {
+        x: Math.floor((event.clientX - container.left) / 32),
+        y: Math.floor((event.clientY - container.top) / 32),
       };
-    } else if (Math.abs(dx) < Math.abs(dy)) {
-      point = {
-        x: point.x,
-        y: startPoint.y + Math.round(dx * Math.tan(a)),
-      };
-    } else {
-      point = {
-        x: startPoint.x + Math.round(dy / Math.tan(a)),
-        y: point.y,
-      };
-    }
 
-    // Limit length
-    dx = point.x - startPoint.x;
-    dy = point.y - startPoint.y;
+      let dx = point.x - startPoint.x;
+      let dy = point.y - startPoint.y;
 
-    if (point.x > cols - 1) {
-      point.x = cols - 1;
-      point.y = startPoint.y + Math.sign(dy) * (cols - 1 - startPoint.x);
-    } else if (point.x < 0) {
-      point.x = 0;
-      point.y = startPoint.y + Math.sign(dy) * startPoint.x;
-    }
+      // Limit angle
+      const a = Math.round(angle(startPoint, point) / (Math.PI / 4)) * (Math.PI / 4);
 
-    if (point.y > rows - 1) {
-      point.x = startPoint.x + Math.sign(dx) * (rows - 1 - startPoint.y);
-      point.y = rows - 1;
-    } else if (point.y < 0) {
-      point.x = startPoint.x + Math.sign(dx) * startPoint.y;
-      point.y = 0;
-    }
+      if (Math.abs(a) === Math.PI / 2) {
+        point = {
+          x: startPoint.x,
+          y: Math.min(Math.max(point.y, 0), rows - 1),
+        };
+      } else if (Math.abs(a) === Math.PI || a === 0) {
+        point = {
+          x: Math.min(Math.max(point.x, 0), cols - 1),
+          y: startPoint.y,
+        };
+      } else if (Math.abs(dx) < Math.abs(dy)) {
+        point = {
+          x: point.x,
+          y: startPoint.y + Math.round(dx * Math.tan(a)),
+        };
+      } else {
+        point = {
+          x: startPoint.x + Math.round(dy / Math.tan(a)),
+          y: point.y,
+        };
+      }
 
-    setEndPoint(point);
-  }, [startPoint]);
+      // Limit length
+      dx = point.x - startPoint.x;
+      dy = point.y - startPoint.y;
+
+      if (point.x > cols - 1) {
+        point.x = cols - 1;
+        point.y = startPoint.y + Math.sign(dy) * (cols - 1 - startPoint.x);
+      } else if (point.x < 0) {
+        point.x = 0;
+        point.y = startPoint.y + Math.sign(dy) * startPoint.x;
+      }
+
+      if (point.y > rows - 1) {
+        point.x = startPoint.x + Math.sign(dx) * (rows - 1 - startPoint.y);
+        point.y = rows - 1;
+      } else if (point.y < 0) {
+        point.x = startPoint.x + Math.sign(dx) * startPoint.y;
+        point.y = 0;
+      }
+
+      setEndPoint(point);
+    },
+    [startPoint, rows, cols],
+  );
 
   const handlePointerUp = useCallback(() => {
     if (startPoint && endPoint) {
@@ -104,31 +110,33 @@ export default function Grid({ className, grid }: GridProps) {
     return () => {
       document.removeEventListener('pointerup', handlePointerUp);
       document.removeEventListener('pointercancel', handlePointerUp);
-    }
+    };
   }, [handlePointerUp]);
 
   return (
     <div
-      className={clsx('relative grid auto-cols-auto auto-rows-auto w-fit select-none', className)}
+      className={clsx('relative grid w-fit auto-cols-auto auto-rows-auto select-none', className)}
       onPointerDown={handlePointerDown}
       onPointerMove={handlePointerMove}
     >
-      {grid.map((row, y) => row.map((cell, x) => (
-        <div
-          key={`${x}:${y}`}
-          className="size-8 text-center leading-8"
-          data-x={x}
-          data-y={y}
-          style={{ gridRow: y + 1, gridColumn: x + 1 }}
-        >
-          {cell}
-        </div>
-      )))}
+      {grid.map((row, y) =>
+        row.map((cell, x) => (
+          <div
+            key={`${x}:${y}`}
+            className="size-8 text-center leading-8"
+            data-x={x}
+            data-y={y}
+            style={{ gridRow: y + 1, gridColumn: x + 1 }}
+          >
+            {cell}
+          </div>
+        )),
+      )}
 
       {overlays.map(([start, end], idx) => (
         <div
           key={idx}
-          className="bg-amber-200/50 h-5 absolute w-full -z-10 pointer-events-none"
+          className="pointer-events-none absolute -z-10 h-5 w-full bg-amber-200/50"
           style={{
             width: `${length(start, end)}px`,
             transform: `translate(${start.x * 32 + 6}px, ${start.y * 32 + 6}px) rotate(${angle(start, end)}rad)`,
@@ -137,9 +145,9 @@ export default function Grid({ className, grid }: GridProps) {
         />
       ))}
 
-      { startPoint && endPoint && (
+      {startPoint && endPoint && (
         <div
-          className="bg-amber-200/50 h-5 absolute w-full -z-10 pointer-events-none"
+          className="pointer-events-none absolute -z-10 h-5 w-full bg-amber-200/50"
           style={{
             width: `${length(startPoint, endPoint)}px`,
             transform: `translate(${startPoint.x * 32 + 6}px, ${startPoint.y * 32 + 6}px) rotate(${angle(startPoint, endPoint)}rad)`,
